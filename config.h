3f1dfc724c03daa59e60871c25f712baaab6d8aa
Grishma Dhakal <dhakalgrishma5@gmail.com>
2024-12-23 21:25:00 +0545

    fix swallow termial and removed some rules

--- config.h
+++ config.h
@@ -31,13 +31,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class                instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "TelegramDesktop",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "obs",                NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Lutris",             NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "firefox",   					NULL,     NULL,           1 << 2,    0,          0,          -1,        -1 },
-	{ "Code",   						NULL,     NULL,           1 << 3,    0,          0,          -1,        -1 },
-	{ "Thunar",   					NULL,     NULL,           1 << 3,    0,          0,          -1,        -1 },
-	{ "st",          				NULL,     NULL,           0,    0,          1,           0,        -1 },
+    { "St",                 NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,                 NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
